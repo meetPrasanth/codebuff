67fe89700187e5bb9c19c0ee21516f744ad88088
Prasanth Venkatesan <onlyyprasanth@gmail.com>
2018-08-03 11:57:17 +0530

    [CodeBuff] Challenges page with hackerearth integration and native calendar with event creation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,61 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
+import { Calendar } from '@ionic-native/calendar';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-    lists = ['ghsdkf', 'sdjhf', 'sdjhgf'];
-  constructor(public navCtrl: NavController) {
+    lists: any;
+    constructor(private http: HTTP, public navCtrl: NavController, private platform: Platform, private calendar: Calendar) {
+        this.platform.ready().then((readySource) => {
+            console.log('Platform ready from', readySource);
+            // Platform now ready, execute any required native code
+            this.mGetHackerearth();
 
-  }
+            this.calendar.listCalendars().then(
+                data => {
+                    console.log("cals", data);
+                }, err => {}
+            );
+            
+        });
+    }
 
+    mCreateEvent(challenge) {
+        this.calendar.hasReadWritePermission().then(
+            data => {
+                console.log("per", data);
+                if(data) {
+                    console.log("per-if", data);
+                    this.calendar.createEventInteractively(challenge.title, challenge.url, challenge.title, new Date(challenge.start_utc_tz), new Date(challenge.end_utc_tz)).then(
+                        event => {
+                            console.log("event", event);
+                        }, eventErr => {
+                            console.log("Event", eventErr);
+                        }
+                    );
+                }else {
+                    this.calendar.requestReadWritePermission().then(
+                        perdata => {
+                            console.log("req", perdata);
+                        }, err => {  console.log("req", err); }
+                    );
+                }
+            }, err => {}
+        );
+    }
+
+    mGetHackerearth() {
+        this.http.get('https://www.hackerearth.com/chrome-extension/events', {}, {})
+            .then(data => {
+                this.lists = JSON.parse(data['data'])['response'];
+                console.log("data", this.lists);
+            }).catch(err => {
+                console.error("error", err);
+            });
+    }
 }
